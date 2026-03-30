b94c148aa432541599f6d07305847156c021f3cb
David Sánchez <dssanchez@estud.usfq.edu.ec>
2026-03-29 20:57:36 -0500

    Se desarrolló el Ejercicio 2

---
+++ Ejercicio_2.py
@@ -0,0 +1,22 @@
+n = 0
+A = []
+while True:
+    try:
+        m = int(input("Ingrese la cantidad de números que va a agregar: "))
+        break
+    except ValueError:
+        print("Valor no valido, digite un número entero.")
+
+while n < m:
+    try:
+        numero = int(input(f"Ingrese número {n + 1}: "))
+        A.append(numero)
+        n += 1
+    except ValueError:
+        print("Valor no valido, digite un número entero.")
+        n = 0
+
+print(f"El mayor es: {max(A)}\nEl menor es: {min(A)}")
+
+#Algoritmo de complejidad O(n) (lineal)
+#Recorre la lista una sola vez para encontrar el número mayor y el número menor.
